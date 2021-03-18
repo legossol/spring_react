5d3fb93093cf98536ca4c0d0ad2a99ab67bb8325
legossol <wkdgothf@gmail.com>
2021-03-18 13:00:24 +0900

    first commit

---
+++ src/main/java/com/example/demo/uss/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.demo.uss.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.uss.domain.User;
+
+
+public interface UserService{
+	
+	
+}
